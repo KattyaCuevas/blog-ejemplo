49feb2842a9934abb161527ca82ad44971a01c17
Kattya Cuevas <kattya.cuevas@gmail.com>
2014-04-04 17:31:50 -0500

    Agregar vista comentarios

--- app/controllers/comentarios_controller.rb
+++ app/controllers/comentarios_controller.rb
@@ -1,7 +1,7 @@
 class ComentariosController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comentario = @post.comentarios.create!(params[:comentario])
+    @comentario = @post.comentarios.create(texto: params[:comentario]['texto'])
     redirect_to @post
   end
 end
